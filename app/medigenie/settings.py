46a2d1d903903728858c7c3d23f25a0f3d2c82ea
Abdul Rafay Atiq <abdulrafayatiq.03@gmail.com>
2025-12-14 15:39:11 +0500

    add csrf and cors config

--- app/medigenie/settings.py
+++ app/medigenie/settings.py
@@ -188,7 +188,9 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://localhost:3000",   
+    "http://localhost:3000",
+    "https://13.233.251.125",
+   
 ]
 
 CORS_ALLOW_HEADERS = [
